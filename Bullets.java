e5869e76439ca493040a706ad7b64f930efd726d
Ethan Dickey <>
2016-05-20 11:56:43 -0500

    Implemented toString(), moveEmAll(), and drawEmAll methods.

--- Bullets.java
+++ Bullets.java
@@ -20,20 +20,23 @@ public class Bullets
     private List<Ammo> ammo;
 
     public Bullets(){
-
+        
     }
     public void add(Ammo al){
-
+        ammo.add(al);
     }
-
     //post - draw each Ammo
     public void drawEmAll(Graphics window){
-
+        ammo.stream().forEach((ab) -> {
+            ab.draw(window);
+        });
     }
     public void moveEmAll(){
-
+        ammo.stream().forEach((ab) -> {
+            ab.move("UP");//doesn't use the string
+        });
     }
-    public void cleanEmUp(){
+    public void cleanEmUp(){ //remove all of the "dead" bullets (with 0 speed)
         for(int ab=0;ab<ammo.size();ab++){
             if(ammo.get(ab).getSpeed()==0){
                 ammo.remove(ab);
@@ -45,6 +48,10 @@ public class Bullets
         return ammo;
     }
     public String toString(){
-        return "";
+        String str = "";
+        for(Ammo ab : ammo){
+            str+=ab.toString()+" ";
+        }
+        return str;
     }
 }
